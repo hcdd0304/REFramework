83a7ec5b5580d487ac91ba311bc06404c678b8b6
hcdd0304 <hcdd0304@proton.me>
2026-01-16 01:40:27 +0700

    Fix

--- src/mods/ScriptRunner.cpp
+++ src/mods/ScriptRunner.cpp
@@ -459,12 +459,18 @@ sol::protected_function_result ScriptState::handle_protected_result(sol::protect
     return result;
 }
 
-bool ScriptState::should_remove_hook(const sol::object &result) {
-    if (!result.valid() || result.is<sol::nil_t>()) {
+bool ScriptState::should_remove_hook(const sol::protected_function_result &result) {
+    if (!result.valid()) {
         return false;
     }
 
-    auto action = result.as<ReCallbackNextAction>();
+    auto result_obj = result.get<sol::object>();
+
+    if (!result_obj.valid() || result_obj.is<sol::nil_t>()) {
+        return false;
+    }
+
+    auto action = result_obj.as<ReCallbackNextAction>();
     return action == ReCallbackNextAction::STOP;
 }
 
@@ -474,7 +480,7 @@ void ScriptState::on_frame() {
 
         auto guard = m_on_frame_fns.acquire_iteration();
         for (auto& fn : m_on_frame_fns.get()) {
-            sol::object result = handle_protected_result(fn());
+            auto result = handle_protected_result(fn());
 
             if (should_remove_hook(result)) {
                 m_on_frame_fns.remove(fn);
@@ -496,7 +502,7 @@ void ScriptState::on_draw_ui() {
 
         auto guard = m_on_draw_ui_fns.acquire_iteration();
         for (auto& fn : m_on_draw_ui_fns.get()) {
-            sol::object result = handle_protected_result(fn());
+            auto result = handle_protected_result(fn());
 
             if (should_remove_hook(result)) {
                 m_on_draw_ui_fns.remove(fn);
@@ -609,8 +615,10 @@ bool ScriptState::on_pre_gui_draw_element(REComponent* gui_element, void* contex
 
         auto guard = m_pre_gui_draw_element_fns.acquire_iteration();
         for (auto& fn : m_pre_gui_draw_element_fns.get()) {
-            if (sol::object result = handle_protected_result(fn(gui_element, context))) {
-                if (!result.is<sol::nil_t>() && result.is<bool>() && result.as<bool>() == false) {
+            if (auto result = handle_protected_result(fn(gui_element, context))) {
+                auto result_obj = result.get<sol::object>();
+
+                if (!result_obj.is<sol::nil_t>() && result_obj.is<bool>() && result_obj.as<bool>() == false) {
                     any_false = true;
                 } else {
                     if (should_remove_hook(result)) {
@@ -634,7 +642,7 @@ void ScriptState::on_gui_draw_element(REComponent* gui_element, void* context) {
 
         auto guard = m_gui_draw_element_fns.acquire_iteration();
         for (auto& fn : m_gui_draw_element_fns.get()) {
-            sol::object result = handle_protected_result(fn(gui_element, context));
+            auto result = handle_protected_result(fn(gui_element, context));
 
             if (should_remove_hook(result)) {
                 m_gui_draw_element_fns.remove(fn);
@@ -653,7 +661,7 @@ void ScriptState::on_script_reset() try {
     // We first call on_config_save functions so scripts can save prior to reset.
     auto guard_save = m_on_config_save_fns.acquire_iteration();
     for (auto& fn : m_on_config_save_fns.get()) {
-        sol::object result = handle_protected_result(fn());
+        auto result = handle_protected_result(fn());
         if (should_remove_hook(result)) {
             m_on_config_save_fns.remove(fn);
         }
@@ -661,7 +669,7 @@ void ScriptState::on_script_reset() try {
 
     auto guard_reset = m_on_script_reset_fns.acquire_iteration();
     for (auto& fn : m_on_script_reset_fns.get()) {
-        sol::object result = handle_protected_result(fn());
+        auto result = handle_protected_result(fn());
         if (should_remove_hook(result)) {
             m_on_script_reset_fns.remove(fn);
         }
@@ -677,7 +685,7 @@ void ScriptState::on_config_save() try {
 
     auto guard = m_on_config_save_fns.acquire_iteration();
     for (auto& fn : m_on_config_save_fns.get()) {
-        sol::object result = handle_protected_result(fn());
+        auto result = handle_protected_result(fn());
 
         if (should_remove_hook(result)) {
             m_on_config_save_fns.remove(fn);
